b01394d24a7157d49b60c2e9c513792050e21e6f
ardhimaulidani <ardhi.maulidani@gmail.com>
2024-03-08 22:42:24 +0700

    gitpages

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,13 +2,13 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
+    title: 'Ardhika Maulidani',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+      'Ardhika Maulidani is a electronics engineer who specializes in robotics and firmware.',
+    siteUrl: 'https://ardhimaulidani.github.io/portfoliov2', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
   },
+  pathPrefix: '/portfoliov2',
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-styled-components`,
